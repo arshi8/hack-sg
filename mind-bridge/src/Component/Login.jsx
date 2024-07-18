5657f1154260b51b1be4d7db23a4634f737903bd
Shraddha Kharat <74046255+Shraddhask19@users.noreply.github.com>
2024-07-18 19:32:25 +0000

    SignUp function added

--- mind-bridge/src/Component/Login.jsx
+++ mind-bridge/src/Component/Login.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
@@ -8,9 +9,33 @@ import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import { Container } from "@mui/material";
+import { Container, Dialog, DialogTitle, DialogContent, DialogActions } from "@mui/material";
 
 export default function Login() {
+  const [showSignUp, setShowSignUp] = useState(false);
+  const [signUpType, setSignUpType] = useState("careSeeker"); // Default to care seeker
+
+  // State for dialog
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const handleSignUpClick = () => {
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
+  const handleSignUpType = (type) => {
+    setSignUpType(type);
+    setOpenDialog(false);
+    setShowSignUp(true);
+  };
+
+  const handleSignInClick = () => {
+    setShowSignUp(false);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -22,11 +47,7 @@ export default function Login() {
 
   return (
     <Container component="main" maxWidth="lg">
-      <Box
-        sx={{
-          marginTop: 8,
-        }}
-      >
+      <Box sx={{ marginTop: 8 }}>
         <Grid container>
           <CssBaseline />
           <Grid
@@ -45,78 +66,116 @@ export default function Login() {
               backgroundPosition: "center",
             }}
           />
-          <Grid
-            item
-            xs={12}
-            sm={8}
-            md={5}
-            component={Paper}
-            elevation={6}
-            square
-          >
-            <Box
-              sx={{
-                my: 8,
-                mx: 4,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
+          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <Box sx={{ my: 8, mx: 4, display: "flex", flexDirection: "column", alignItems: "center" }}>
               <Typography component="h1" variant="h5">
-                Sign in
+                {showSignUp ? "Sign Up" : "Sign in"}
               </Typography>
-              <Box
-                component="form"
-                noValidate
-                onSubmit={handleSubmit}
-                sx={{ mt: 1 }}
-              >
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  autoFocus
-                />
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
-                <FormControlLabel
-                  control={<Checkbox value="remember" color="primary" />}
-                  label="Remember me"
-                />
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  Sign In
-                </Button>
-                <Grid container>
-                  <Grid item xs>
-                    <Link href="#" variant="body2">
-                      Forgot password?
-                    </Link>
+              {showSignUp ? (
+                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                  />
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                  />
+                  {signUpType === "careTaker" && (
+                    <TextField
+                      margin="normal"
+                      required
+                      fullWidth
+                      id="careSeekerId"
+                      label="Care Seeker ID"
+                      name="careSeekerId"
+                      autoComplete="careSeekerId"
+                    />
+                  )}
+                  <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+                    Sign Up
+                  </Button>
+                  <Grid container justifyContent="flex-end">
+                    <Grid item>
+                      <Link href="#" variant="body2" onClick={handleSignInClick}>
+                        Already have an account? Sign in
+                      </Link>
+                    </Grid>
                   </Grid>
-                  <Grid item>
-                    <Link href="#" variant="body2">
-                      {"Don't have an account? Sign Up"}
-                    </Link>
+                </Box>
+              ) : (
+                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                  />
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox value="remember" color="primary" />}
+                    label="Remember me"
+                  />
+                  <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+                    Sign In
+                  </Button>
+                  <Grid container>
+                    <Grid item xs>
+                      <Link href="#" variant="body2">
+                        Forgot password?
+                      </Link>
+                    </Grid>
+                    <Grid item>
+                      <Link href="#" variant="body2" onClick={handleSignUpClick}>
+                        Don't have an account? Sign Up
+                      </Link>
+                    </Grid>
                   </Grid>
-                </Grid>
-              </Box>
+                </Box>
+              )}
+
+              {/* Sign Up Dialog */}
+              <Dialog open={openDialog} onClose={handleCloseDialog}>
+                <DialogTitle>Choose Sign Up Option</DialogTitle>
+                <DialogContent>
+                  <Button fullWidth variant="contained" color="primary" onClick={() => handleSignUpType("careSeeker")} sx={{ mb: 1 }}>
+                    Sign Up as Care Seeker
+                  </Button>
+                  <Button fullWidth variant="contained" color="primary" onClick={() => handleSignUpType("careTaker")} sx={{ mb: 1 }}>
+                    Sign Up as Care Taker
+                  </Button>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleCloseDialog} color="secondary">
+                    Cancel
+                  </Button>
+                </DialogActions>
+              </Dialog>
             </Box>
           </Grid>
         </Grid>
